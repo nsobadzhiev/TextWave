28d9ea94061b6734270396db4ee5b9cc0dbe91c5
Niki <nikola@devmonologue.com>
2016-12-03 22:45:04 +0100

    Changed TWSourcesCollectionViewCell's shake animation

--- TextWave/UI/TWSourcesCollectionViewCell.swift
+++ TextWave/UI/TWSourcesCollectionViewCell.swift
@@ -36,58 +36,15 @@ class TWSourcesCollectionViewCell : UICollectionViewCell {
     }
     
     func startShaking() {
-        let delay = Double(Double(arc4random_uniform(10)) / 10.0)
-        //let movement = CGFloat(arc4random_uniform(5))
-        let animationOptions:UIViewKeyframeAnimationOptions = [.allowUserInteraction, .repeat]
-        let easeOptions:UIViewAnimationOptions = .repeat
-        UIView.animate(withDuration: 0.5, delay: delay, options: easeOptions, animations: { () -> Void in
-            UIView.animateKeyframes(withDuration: 0.60, delay: delay, options: animationOptions, animations: { () -> Void in
-                var time = 0.0
-                let duration = 0.15
-                let radius:CGFloat = CGFloat(M_PI / 200.0);
-                let originalFrame = self.frame
-                UIView.addKeyframe(withRelativeStartTime: time, relativeDuration: duration, animations: { () -> Void in
-                    let transform = CGAffineTransform(rotationAngle: -radius);
-                    self.transform = transform;
-                    var myFrame = self.frame
-                    let movementX = -5.0 + CGFloat(arc4random_uniform(10))
-                    let movementY = -5.0 + CGFloat(arc4random_uniform(10))
-                    myFrame.origin.x += movementX
-                    myFrame.origin.y += movementY
-                    self.frame = myFrame
-                })
-                time += duration
-                UIView.addKeyframe(withRelativeStartTime: time, relativeDuration: duration, animations: { () -> Void in
-                    let transform = CGAffineTransform(rotationAngle: radius);
-                    self.transform = transform;
-                    var myFrame = self.frame
-                    let movementX = -5.0 + CGFloat(arc4random_uniform(10))
-                    let movementY = -5.0 + CGFloat(arc4random_uniform(10))
-                    myFrame.origin.x += movementX
-                    myFrame.origin.y += movementY
-                    self.frame = myFrame
-                })
-                time += duration
-                UIView.addKeyframe(withRelativeStartTime: time, relativeDuration: duration, animations: { () -> Void in
-                    let transform = CGAffineTransform(rotationAngle: -radius);
-                    self.transform = transform;
-                    var myFrame = self.frame
-                    let movementX = -5.0 + CGFloat(arc4random_uniform(10))
-                    let movementY = -5.0 + CGFloat(arc4random_uniform(10))
-                    myFrame.origin.x += movementX
-                    myFrame.origin.y += movementY
-                    self.frame = myFrame
-                })
-                time += duration
-                UIView.addKeyframe(withRelativeStartTime: time, relativeDuration: duration, animations: { () -> Void in
-                    let transform = CGAffineTransform(rotationAngle: 0);
-                    self.transform = transform;
-                    self.frame = originalFrame
-                })
-                time += duration
-                }, completion: nil)
-            }, 
-            completion: nil)
+        let animation = CAKeyframeAnimation(keyPath: "transform")
+        animation.values = [NSValue(caTransform3D:CATransform3DMakeTranslation(-2.0, 0.0, 0.0)),
+        NSValue(caTransform3D:CATransform3DMakeTranslation(2.0, 0.0, 0.0)),
+        NSValue(caTransform3D:CATransform3DMakeRotation((CGFloat) (-1.0/180.0 * M_PI), 0, 0, 1)),
+        NSValue(caTransform3D:CATransform3DMakeRotation((CGFloat) (1.0/180.0 * M_PI), 0, 0, 1))]
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        animation.duration = 0.3
+        self.layer.add(animation, forKey: nil)
     }
     
     func stopShaking() {
